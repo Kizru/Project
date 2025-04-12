fe98054513fb575dc3fd0ec1997aea9d06851bbc
Kizru <119551650+Kizru@users.noreply.github.com>
2025-04-12 15:57:27 +0500

    Добавлена надпись и кнопка для смены темы

--- Work.py
+++ Work.py
@@ -3,14 +3,20 @@ from flet.core.icons import Icons
 import requests
 
 def main(page: ft.Page):
-    page.title = "Сборка ПК"
+    page.title = "Конфигуратор ПК"
     page.theme_mode = 'light'
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
     page.update()
+
+    def changeTheme(e):
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
+        page.update()
 
     page.add(
 
+        #смена темы
+        ft.Row([ft.IconButton(Icons.SUNNY, on_click = changeTheme)], alignment = ft.MainAxisAlignment.START), 
+
+        ft.Row([ft.Text("Конфигуратор ПК")], alignment = ft.MainAxisAlignment.CENTER) 
     )
 
 # Запускаем приложение
